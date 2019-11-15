532c74f933c418ce1e7a43ac60e29051a4e2e909
Wesley Urban <wes.urb@gmail.com>
2019-11-15 15:45:49 +0100

    update

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+
 app.use(express.json())
 app.use(userRouter, taskRouter)
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/user')
+const auth = require('../middleware/auth') //middleware for auth with token
 
 const router = new express.Router()
 
@@ -24,13 +25,31 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-router.get('/users', async (req, res) => {
+router.post('/users/logout', auth, async (req, res) => {
     try {
-        const users = await User.find({})
-        res.send(users)
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+
+        res.send()
     } catch (e) {
         res.status(500).send()
     }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user) //from auth
 })
 
 router.get('/users/:id', async (req, res) => {
