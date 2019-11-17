c8c072ae46c2983610e69c32eb1f99f110e613ba
Wesley <wes.urb@gmail.com>
2019-11-17 14:11:39 +0100

    update

--- src/index.js
+++ src/index.js
@@ -17,14 +17,14 @@ app.listen(port, () => {
 const Task = require('./models/task')
 const User = require('./models/user')
 
-const main = async () => {
-    // const task = await Task.findById('5dd13646f8093b05043ecb26')
-    // await task.populate('owner').execPopulate() 
-    // console.log(task.owner.name)
+// const main = async () => {
+//     // const task = await Task.findById('5dd13646f8093b05043ecb26')
+//     // await task.populate('owner').execPopulate() 
+//     // console.log(task.owner.name)
 
-    const user = await User.findById('5dd13a520629ee1ab0150a76')
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
-}
+//     const user = await User.findById('5dd13a520629ee1ab0150a76')
+//     await user.populate('tasks').execPopulate()
+//     console.log(user.tasks)
+// }
 
-main()
+// main()
