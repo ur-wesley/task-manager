f380a328df676e69ba8078295fede60e1cfcc9f9
Wesley Urban <wes.urb@gmail.com>
2019-11-21 15:42:17 +0100

    frontend

---
+++ frontend/src/components/ListTodo.vue
@@ -0,0 +1,79 @@
+<template>
+  <div v-bind:show="todos.length>0" class="col align-self-center">
+    <div class="form-row align-items-center">
+      <div class="col-auto my-1">
+        <div class="input-group mb-3 todo__row">
+          <div class="input-group-prepend">
+            <span class="input-group-text">
+              <input
+                type="checkbox"
+                v-model="todo.done"
+                :checked="todo.done"
+                :value="todo.done"
+                v-on:change="updateTodo(todo)"
+                title="Mark as done?"
+              />
+            </span>
+          </div>
+          <input
+            type="text"
+            class="form-control"
+            :class="todo.done?'todo__done':''"
+            v-model="todo.name"
+            @keypress="todo.editing=true"
+            @keyup.enter="updateTodo(todo)"
+          />
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span
+                class="input-group-addon addon-left"
+                title="Delete todo?"
+                v-on:click="deleteTodo(todo._id)"
+              >
+                X
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      class="alert alert-primary todo__row"
+      v-show="todos.length==0 && doneLoading"
+    >Hardest worker in the room. No more todos now you can rest. ;)</div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+.todo__done {
+  text-decoration: line-through !important;
+}
+
+.no_border_left_right {
+  border-left: 0px;
+  border-right: 0px;
+}
+
+.flat_form {
+  border-radius: 0px;
+}
+
+.mrb-10 {
+  margin-bottom: 10px;
+}
+
+.addon-left {
+  background-color: none !important;
+  border-left: 0px !important;
+  cursor: pointer !important;
+}
+
+.addon-right {
+  background-color: none !important;
+  border-right: 0px !important;
+}
+</style>
---
+++ frontend/src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+import axios from 'axios';
+import App from './App.vue';
+
+const http = axios.create({
+  baseURL: process.env.BACKEND_URL ? process.env.BACKEND_URL : 'http://localhost',
+});
+
+Vue.prototype.$http = http;
+
+Vue.use(BootstrapVue);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: (h) => h(App),
+}).$mount('#app');
