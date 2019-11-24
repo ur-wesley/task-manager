7252f58af17f06763ac70b5cbf13158e04524f7b
Wesley <wes.urb@gmail.com>
2019-11-24 11:43:04 +0100

    update

--- src/routers/user.js
+++ src/routers/user.js
@@ -16,7 +16,7 @@ router.post("/users", async (req, res) => {
     const token = await user.generateAuthToken()
     res.status(201).send({ user, token })
   } catch (e) {
-    res.status(400).send({ error: e })
+    res.status(400).send({ e })
   }
 })
 
